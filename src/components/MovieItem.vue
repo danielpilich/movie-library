2f76ae7b381575d2ba1336155c09e21b415e3793
Daniel Pilich <dpilich16@gmail.com>
2024-11-25 16:33:13 +0100

    Add delete confirmation box

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,21 +1,33 @@
-<script>
- export default {
-    props: ['movie'],
-
-    methods: {
-    removeMovie() {
-      this.$emit('remove-movie', this.movie.id);
-    }
-  }
- }
-</script>
-
 <template>
   <div id="movie">
     <h2>{{ movie.title }}</h2>
+    <h2>{{ movie.id }}</h2>
     <div class="button-container">
-        <button @click="editMovie">Edit</button>
-        <button @click="removeMovie">Remove</button>
+      <button @click="editMovie">Edit</button>
+      <button @click="showRemoveModal = true">Remove</button>
     </div>
+    <RemoveModalBox v-if="showRemoveModal" :show="showRemoveModal" :movie="movie" @confirm-remove="removeMovie" @cancel="showRemoveModal = false" />
   </div>
 </template>
+
+<script>
+import RemoveModalBox from './RemoveModalBox.vue';
+
+export default {
+  components: { RemoveModalBox },
+  props: ['movie'],
+  data() {
+    return {
+      showRemoveModal: false
+    }
+  },
+  methods: {
+    removeMovie() {
+      this.$emit('remove-movie', this.movie.id);
+    },
+    editMovie() {
+      this.$emit('edit-movie', this.movie);
+    }
+  }
+}
+</script>
---
+++ src/components/RemoveModalBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal-overlay" v-if="show">
+    <div class="modal">
+      <h2>Confirm Removal</h2>
+      <p>Are you sure you want to remove {{ movie.title }}?</p>
+      <button @click="confirmRemove">Yes, remove</button>
+      <button @click="cancel">Cancel</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['show', 'movie'],
+  methods: {
+    confirmRemove() {
+      this.$emit('confirm-remove');
+    },
+    cancel() {
+      this.$emit('cancel');
+    }
+  }
+}
+</script>
