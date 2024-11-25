022e7582f2d9f8e1200114926c0f4c94846ce3ae
Daniel Pilich <dpilich16@gmail.com>
2024-11-25 19:08:23 +0100

    Add basic form validations

--- src/components/AddMovieModal.vue
+++ src/components/AddMovieModal.vue
@@ -5,6 +5,9 @@
       <form @submit.prevent="addMovie">
         <label for="title">Title:</label>
         <input type="text" id="title" v-model="newMovie.title" />
+        <div v-if="v$.newMovie.title.$error">
+          {{ v$.newMovie.title.$errors[0].$message }}
+        </div>
         <button type="submit">Add Movie</button>
         <button @click="cancel">Cancel</button>
       </form>
@@ -13,6 +16,9 @@
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
 export default {
   props: ['show'],
   data() {
@@ -22,13 +28,26 @@ export default {
       }
     }
   },
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  validations () {
+    return {
+      newMovie: {
+        title: { required }
+      }
+    }
+  },
   methods: {
     addMovie() {
-      this.$emit('add-movie', this.newMovie);
-      this.newMovie.title = '';
+      this.v$.$validate()
+      if (!this.v$.$error) {
+        this.$emit('add-movie', this.newMovie)
+        this.newMovie.title = ''
+      }
     },
     cancel() {
-      this.$emit('cancel');
+      this.$emit('cancel')
     }
   }
 }
--- src/components/EditMovieModal.vue
+++ src/components/EditMovieModal.vue
@@ -5,6 +5,9 @@
       <form @submit.prevent="saveMovie">
         <label for="title">Title:</label>
         <input type="text" id="title" v-model="editedMovie.title" />
+        <div v-if="v$.editedMovie.title.$error">
+          {{ v$.editedMovie.title.$errors[0].$message }}
+        </div>
         <button type="submit">Save Changes</button>
         <button @click="cancel">Cancel</button>
       </form>
@@ -13,24 +16,45 @@
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
 export default {
   props: {
     show: Boolean,
     movie: Object
   },
+  setup () {
+    return { v$: useVuelidate() }
+  },
   data() {
     return {
-      editedMovie: { ...this.movie }
+      editedMovie: {
+        title: ''
+      },
+    }
+  },
+  validations() {
+    return {
+      editedMovie: {
+        title: { required }
+      }
     }
   },
   methods: {
     saveMovie() {
-      this.$emit('edit-movie', this.editedMovie);
-      this.cancel();
+      this.v$.$validate()
+      if (!this.v$.$error) {
+        this.$emit('edit-movie', this.editedMovie)
+        this.cancel()
+      }
     },
     cancel() {
-      this.$emit('cancel');
+      this.$emit('cancel')
     }
+  },
+  mounted() {
+    this.editedMovie = { ...this.movie }
   }
 }
 </script>
